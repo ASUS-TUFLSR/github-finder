a5963ee420cb632531a9a1e652ae68bec2199a26
Manmit Pal <manmitpal348@gmail.com>
2024-03-24 20:34:28 +0530

    Added end point users

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './users/Users';
+import axios from 'axios';
 
 class App extends Component {
+
+state = {
+  users: [],
+  loading: false
+}
+
+  async componentDidMount() {
+    this.setState({loading:true})
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({users:res.data, loading:false})
+    console.log(res.data)
+  }
   
   render(){
       return(
     <div className="App">
    <Navbar/>
    <div className='container'>
-       <Users/>
+       <Users loading={this.state.loading} users={this.state.users}/>
    </div>
    
   </div>
--- src/users/Users.js
+++ src/users/Users.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Useritem from './Useritem'
+import Spinner from '../components/layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
+const Users = ({users, loading}) =>  {
 
-    state = {
-        users: [ 
-    {   id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    },
-    {   id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-    },
-    {   id: '3',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-    },
-]
+    if(loading){
+        <Spinner/>
+    }else{
+        return (
+            <div style={userStyle}>
+              {users.map(user => (
+                  <Useritem key={user.id} user={user}/>
+              ))}
+            </div>
+          )
     }
-  render() {
+   
+  
+}
 
-    return (
-      <div style={userStyle}>
-        {this.state.users.map(user => (
-            <Useritem key={user.id} user={user}/>
-        ))}
-      </div>
-    )
-  }
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
