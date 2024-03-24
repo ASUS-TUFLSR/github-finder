d3e51e3c4e3a57a147663cf16bf66391ebe21479
Manmit Pal <manmitpal348@gmail.com>
2024-03-24 15:26:53 +0530

    Github app key added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './users/Users';
 
-function App() {
-  return (
+class App extends Component {
+  
+  render(){
+      return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <Navbar/>
+   <div className='container'>
+       <Users/>
+   </div>
+   
+  </div>
+      );
+    }
 }
 
 export default App;
